339023e269a960c3c42526336fcce104d9f498fa
yongjun18 <62463863+yongjun18@users.noreply.github.com>
2020-06-16 19:07:14 +0900

    Update 10265.cpp

--- BOJ/10200/10265.cpp
+++ BOJ/10200/10265.cpp
@@ -4,69 +4,55 @@ typedef long long ll;
 const int INF = 1e9;
 const int MOD = 1e9+7;
 
-int fward[1010];
-vector<int> bward[1010];
-int check[1010];
+int adj[1010];
+int sink[1010];
 int depth[1010];
+int minsiz[1010], maxsiz[1010];
 int dp[1010];
 
-int last;
-int minsiz, maxsiz;
-
-int dfs2(int now){
-    int ret = 1;
-    for(int pv : bward[now]){
-        if(check[pv] == 0){
-            check[pv] = 1;
-            ret += dfs2(pv);
-        }
-    }
-    return ret;
-}
 void dfs(int now){
-    int nxt = fward[now];
-    if(depth[nxt] == -1){
-        depth[nxt] = depth[now] + 1;
-        dfs(nxt);
+    int nxt = adj[now];
+    if(sink[nxt] != 0){
+        sink[now] = sink[nxt];
+        maxsiz[sink[now]] += depth[now];
+        return;
     }
-    else{
-        minsiz = depth[now] - depth[nxt] + 1;
-        last = now;
+    if(depth[nxt] != 0){
+        sink[now] = now;
+        minsiz[now] = depth[now]-depth[nxt]+1;
+        maxsiz[now] = depth[now];
+        return;
     }
-}
-void update(){
-    for(int i=1009; i>=0; i--){
-        if(dp[i] == 0) continue;
-        for(int j=minsiz; j<=maxsiz; j++)
-            dp[i+j] = 1;
-    }
+    depth[nxt] = depth[now]+1;
+    dfs(nxt);
+    sink[now] = sink[nxt];
 }
 
 int main()
 {
-    int n, k;
+    int n, k, ans;
     scanf("%d %d", &n, &k);
+    for(int i=1; i<=n; i++)
+        scanf("%d", &adj[i]);
+
     for(int i=1; i<=n; i++){
-        scanf("%d", &fward[i]);
-        bward[fward[i]].push_back(i);
+        if(depth[i] == 0){
+            depth[i] = 1;
+            dfs(i);
+        }
     }
-    memset(depth, -1, sizeof(depth));
     dp[0] = 1;
     for(int i=1; i<=n; i++){
-        if(check[i] == 0){
-            depth[i] = 0;
-            dfs(i);
-
-            check[last] = 1;
-            maxsiz = dfs2(last);
-            update();
+        if(sink[i] != i) continue;
+        for(int j=1009; j>=0; j--){
+            if(dp[j] != 0){
+                for(int siz=minsiz[i]; siz<=maxsiz[i]; siz++)
+                    dp[j+siz] = 1;
+            }
         }
     }
-    for(int i=k; i>=0; i--){
-        if(dp[i] == 1){
-            printf("%d\n", i);
-            break;
-        }
-    }
+    ans = k;
+    while(dp[ans] == 0) ans--;
+    printf("%d\n", ans);
     return 0;
 }
