4a92f47b164669eed26f0f979651a433d8651c5a
yongjun18 <62463863+yongjun18@users.noreply.github.com>
2020-08-25 22:31:47 +0900

    Update 10538_hashing.cpp

--- BOJ/10500/10538_hashing.cpp
+++ BOJ/10500/10538_hashing.cpp
@@ -3,94 +3,103 @@ using namespace std;
 typedef long long ll;
 const int INF = 1e9;
 const int MOD = 1e9+7;
-const int MX = 2005;
 
 struct Hash{
-    ll power;
-    ll powv[MX];
-    Hash(int p){
-        power = p;
+    static const int MX = 2005;
+    ll power, powv[MX];
+    int len;
+    vector<ll> st, val;
+
+    void init(ll power, int len){
+        this->power = power;
+        this->len = len;
         powv[0] = 1;
         for(int i=1; i<MX; i++)
             powv[i] = (powv[i-1] * power) % MOD;
     }
-    ll compute(vector<ll>& arr){
-        ll ret = 0;
-        for(int i=0; i<arr.size(); i++){
-            ret = (ret * power) % MOD;
-            ret = (ret + arr[i]) % MOD;
-        }
-        return ret;
+
+    ll get_val(){
+        if(st.size() >= len)
+            return val.back();
+        else return -1;
     }
-    ll next(int siz, ll phash, ll delv, ll newv){
-        ll ret = phash;
-        ret = ((ret - delv * powv[siz-1] % MOD) + MOD) % MOD;
-        ret = (ret * power) % MOD;
-        ret = (ret + newv) % MOD;
-        return ret;
+
+    void insert(ll x){
+        ll nxt;
+        if(st.size() == 0) nxt = 0;
+        else nxt = val.back();
+
+        if(st.size() >= len){
+            ll del = st[st.size()-len];
+            nxt = ((nxt - del * powv[len-1] % MOD) + MOD) % MOD;
+        }
+        nxt = (nxt * power) % MOD;
+        nxt = (nxt + x) % MOD;
+        st.push_back(x);
+        val.push_back(nxt);
+    }
+
+    void pop(){
+        st.pop_back();
+        val.pop_back();
+    }
+
+    void clear(){
+        st.clear();
+        val.clear();
     }
 };
-Hash hsh1(3), hsh2(7);
-int h1, w1, h2, w2;
-char small[2005][2005];
-char big[2005][2005];
+Hash hs1, hs2;
 
-ll shash;
-ll bhash1[2005][2005];
-ll bhash2[2005][2005];
-
-void compute_shash(){
-    vector<ll> v1, v2;
-    for(int i=0; i<h1; i++){
-        v1.clear();
-        for(int j=0; j<w1; j++)
-            v1.push_back(small[i][j]);
-        v2.push_back(hsh1.compute(v1));
-    }
-    shash = hsh2.compute(v2);
-}
-void compute_bhash(){
-    vector<ll> v1;
-    for(int i=0; i<h2; i++){
-        v1.clear();
-        for(int j=0; j<w1; j++)
-            v1.push_back(big[i][j]);
-        bhash1[i][w1-1] = hsh1.compute(v1);
-
-        for(int j=w1; j<w2; j++)
-            bhash1[i][j] = hsh1.next(w1, bhash1[i][j-1], big[i][j-w1], big[i][j]);
-    }
-    for(int j=w1-1; j<w2; j++){
-        v1.clear();
-        for(int i=0; i<h1; i++)
-            v1.push_back(bhash1[i][j]);
-        bhash2[h1-1][j] = hsh2.compute(v1);
-
-        for(int i=h1; i<h2; i++)
-            bhash2[i][j] = hsh2.next(h1, bhash2[i-1][j], bhash1[i-h1][j], bhash1[i][j]);
-    }
-}
-int compute_ans(){
-    int ans = 0;
-    for(int i=h1-1; i<h2; i++){
-        for(int j=w1-1; j<w2; j++){
-            if(shash == bhash2[i][j])
-                ans++;
-        }
-    }
-    return ans;
-}
+char p1[2010][2010];
+char p2[2010][2010];
+ll hashv1;
+ll hashv2[2010][2010];
 
 int main()
 {
+    int h1, w1, h2, w2;
+    int ans;
     scanf("%d %d %d %d", &h1, &w1, &h2, &w2);
-    for(int i=0; i<h1; i++)
-        scanf(" %s", small[i]);
-    for(int i=0; i<h2; i++)
-        scanf(" %s", big[i]);
 
-    compute_shash();
-    compute_bhash();
-    printf("%d\n", compute_ans());
+    for(int i=0; i<h1; i++)
+        scanf("%s", p1[i]);
+    for(int i=0; i<h2; i++)
+        scanf("%s", p2[i]);
+
+    hs1.init(13, w1);
+    hs2.init(17, h1);
+
+    for(int i=0; i<h1; i++){
+        hs1.clear();
+        for(int j=0; j<w1; j++)
+            hs1.insert(p1[i][j]);
+        hs2.insert(hs1.get_val());
+    }
+    hashv1 = hs2.get_val();
+
+    for(int i=0; i<h2; i++){
+        hs1.clear();
+        for(int j=0; j<w2; j++){
+            hs1.insert(p2[i][j]);
+            hashv2[i][j] = hs1.get_val();
+        }
+    }
+    for(int j=0; j<w2; j++){
+        hs2.clear();
+        for(int i=0; i<h2; i++){
+            hs2.insert(hashv2[i][j]);
+            hashv2[i][j] = hs2.get_val();
+        }
+    }
+
+    ans = 0;
+    for(int i=0; i<h2; i++){
+        for(int j=0; j<w2; j++){
+            if(hashv1 == hashv2[i][j])
+                ans++;
+        }
+    }
+    printf("%d\n", ans);
     return 0;
 }
