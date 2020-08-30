ece39dc84a064676981c2124f47409362b92de72
yongjun18 <62463863+yongjun18@users.noreply.github.com>
2020-08-30 12:56:17 +0900

    Create 1-eratosthenes-sieve.cpp

---
+++ Teamnote/1-Math/1-eratosthenes-sieve.cpp
@@ -0,0 +1,17 @@
+// minf[x] : x의 소인수 중 가장 작은 값
+int minf[1000001];
+
+// O(NlglgN)
+void eratos(int n) {
+	minf[0] = minf[1] = -1;
+	for (int i = 2; i <= n; ++i) minf[i] = i;
+	int sqrtn = (int)sqrt((double)n);
+
+	for (int i = 2; i <= sqrtn; ++i) {
+		if (minf[i] == i) {
+			for (int j = i * i; j <= n; j += i) {
+				if (minf[j] == j) minf[j] = i;
+			}
+		}
+	}
+}
