9b05b52ce1b5b54bea41696b9d7b1e2d9d032ee3
yongjun18 <62463863+yongjun18@users.noreply.github.com>
2020-10-27 19:25:21 +0900

    Update 6-fenwick-tree.cpp

--- Teamnote/3-Data-Structure/6-fenwick-tree.cpp
+++ Teamnote/3-Data-Structure/6-fenwick-tree.cpp
@@ -1,28 +1,27 @@
+const int MX = 1000100;
 int fenn;
 // 반드시 1-indexed array를 사용해야 함
-// fenwick[i] : i번 원소까지 (i & -i)개 원소들에 더해져 있는 값
-ll fenwick[100001];
+ll tree[MX];
 
-// x번 원소의 값 쿼리
-ll query(int x) {
-	ll sum = 0;
-	while (x <= fenn) {
-		sum += fenwick[x];
-		x += (x & -x);
-	}
-	return sum;
-}
-
-// [1, x] 구간에 val 값을 더한다.
-void update(int x, ll val) {
+// [1, x] 구간합
+ll sum(int x) {
+	ll ret = 0;
 	while (x > 0) {
-		fenwick[x] += val;
+		ret += tree[x];
 		x -= (x & -x);
 	}
+	return ret;
 }
 
-// [x, y] 구간에 val값을 더한다.
-void update(int x, int y, ll val) {
-	update(y, val);
-	update(x - 1, -val);
+// [x, y] 구간합
+ll sum(int x, int y){
+	return sum(y) - sum(x - 1);
+}
+
+// x 위치에 diff를 더한다
+void update(int x, ll diff) {
+	while (x <= fenn) {
+		tree[x] += diff;
+		x += (x & -x);
+	}
 }
