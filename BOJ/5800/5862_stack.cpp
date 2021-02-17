4239d9ba2004d90596ecece3c55ddcd5f3573448
yongjun18 <62463863+yongjun18@users.noreply.github.com>
2021-02-17 20:31:30 +0900

    Update 5862_stack.cpp
    lower_bound 를 이용하여 더 간단하게

--- BOJ/5800/5862_stack.cpp
+++ BOJ/5800/5862_stack.cpp
@@ -1,10 +1,13 @@
 #include <stdio.h>
+#include <algorithm>
+#include <vector>
 #include <memory.h>
 #include <stack>
 #include <queue>
-#include <deque>
-#include <algorithm>
-#include <vector>
+#include <map>
+#include <set>
+#include <string.h>
+#include <string>
 using namespace std;
 typedef long long ll;
 const int INF = 1e9;
@@ -12,86 +15,60 @@ const int MOD = 1e9 + 7;
 
 int n;
 ll l;
-vector< pair<int, ll> > adj[200100];
-int siz[200100];
 
-ll dist[200100];
+struct Edge{
+	int to;
+	ll w;
+};
+vector<Edge> adj[200100];
+
+int st[200100];
+ll depth[200100];
 
 int check[200100];
+int dp[200100];
+int siz[200100];
 
-int stk[200100];
-ll stkd[200100];
-int ssiz = 0;
-
-int exc[200100];
-int ans[200100];
-
-void dfs(int now){
-	siz[now] = 1;
-
-	for (auto it : adj[now]){
-		int nxt = it.first;
-		dist[nxt] = dist[now] + it.second;
-		dfs(nxt);
-		siz[now] += siz[nxt];
-	}
-}
-
-ll parametric_search(ll x, ll y, int now){
-	ll lo = x - 1;
-	ll hi = y + 1;
-	while (lo + 1 < hi){
-		ll mid = (lo + hi) / 2;
-		if (dist[now] - dist[stk[mid]] <= l) hi = mid;
-		else lo = mid;
-	}
-	return hi;
-}
-
-void dfs2(int now){
-	stk[ssiz] = now;
-	stkd[ssiz] = dist[now];
-	ssiz++;
-
-	int pos = parametric_search(0, ssiz - 1, now);
+void dfs(int now, int v){
+	int pos = lower_bound(depth, depth + now, depth[now] - l) - depth;
 	if (pos > 0){
-		check[stk[pos - 1]]++;
+		check[st[pos - 1]]++;
 	}
-	for (auto it : adj[now]){
-		dfs2(it.first);
-	}
-
-	ssiz--;
-}
-
-void dfs3(int now){
-	exc[now] = check[now];
-
-	for (auto it : adj[now]){
-		int nxt = it.first;
-		dfs3(nxt);
-		exc[now] += exc[nxt];
+	for (Edge e : adj[v]){
+		st[now + 1] = e.to;
+		depth[now + 1] = depth[now] + e.w;
+		dfs(now + 1, e.to);
 	}
 }
 
+void dfs2(int v){
+	siz[v] = 1;
+	dp[v] = check[v];
+	
+	for (Edge e : adj[v]){
+		dfs2(e.to);
+		siz[v] += siz[e.to];
+		dp[v] += dp[e.to];
+	}
+}
 
 int main()
-{	
+{
 	scanf("%d %lld", &n, &l);
-
 	for (int i = 2; i <= n; i++){
 		int p;
-		ll x;
-		scanf("%d %lld", &p, &x);
-		adj[p].push_back({ i, x });
+		ll w;
+		scanf("%d %lld", &p, &w);
+		adj[p].push_back({ i, w });
 	}
 
-	dfs(1);
+	st[0] = 1;
+	depth[0] = 0;
+	dfs(0, 1);
 	dfs2(1);
-	dfs3(1);
 
 	for (int i = 1; i <= n; i++){
-		printf("%d\n", siz[i] - exc[i]);
+		printf("%d\n", siz[i] - dp[i]);
 	}
 	return 0;
 }
