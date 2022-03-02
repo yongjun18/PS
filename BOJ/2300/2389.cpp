72725c2bb12fc3b3738a52c5a6103d08a2eec37a
yongjun18 <62463863+yongjun18@users.noreply.github.com>
2022-03-02 20:36:41 +0900

    Update 2389.cpp

--- BOJ/2300/2389.cpp
+++ BOJ/2300/2389.cpp
@@ -14,7 +14,7 @@ typedef long long ll;
 const int INF = 1e9;
 const int MOD = 1e9 + 7;
 
-struct vec{
+struct vec {
 	double x, y;
 	explicit vec(double x, double y) { this->x = x; this->y = y; }
 	vec() { x = 0; y = 0; }
@@ -24,22 +24,16 @@ struct vec{
 	double norm() const { return hypot(x, y); }
 };
 
-// https://m.blog.naver.com/hileejaeho/221751241141
-vec center(vec pt1, vec pt2, vec pt3){
-	double A = pt2.x - pt1.x;
-	double B = pt2.y - pt1.y;
-	double C = pt3.x - pt1.x;
-	double D = pt3.y - pt1.y;
-	double E = A * (pt1.x + pt2.x) + B * (pt1.y + pt2.y);
-	double F = C * (pt1.x + pt3.x) + D * (pt1.y + pt3.y);
-	double G = 2.0 * (A * (pt3.y - pt2.y) - B * (pt3.x - pt2.x));
-	if (G == 0) return pt1;
+vec center(vec pt1, vec pt2, vec pt3) {
+	double tmpx1 = 2 * (pt1.x - pt2.x) * (pt3.y - pt2.y) - 2 * (pt3.x - pt2.x) * (pt1.y - pt2.y);
+	double tmpx2 = (pt2.x * pt2.x - pt1.x * pt1.x + pt2.y * pt2.y - pt1.y * pt1.y) * (pt3.y - pt2.y)
+		- (pt2.x * pt2.x - pt3.x * pt3.x + pt2.y * pt2.y - pt3.y * pt3.y) * (pt1.y - pt2.y);
+	double tmpy1 = 2 * (pt1.y - pt2.y) * (pt3.x - pt2.x) - 2 * (pt3.y - pt2.y) * (pt1.x - pt2.x);
+	double tmpy2 = (pt2.y * pt2.y - pt1.y * pt1.y + pt2.x * pt2.x - pt1.x * pt1.x) * (pt3.x - pt2.x)
+		- (pt2.y * pt2.y - pt3.y * pt3.y + pt2.x * pt2.x - pt3.x * pt3.x) * (pt1.x - pt2.x);
 
-	vec ret;
-	ret.x = (D * E - B * F) / G;
-	ret.y = (A * F - C * E) / G;
-
-	return ret;
+	if (tmpx1 == 0 || tmpy1 == 0) return pt1;
+	return vec(- tmpx2 / tmpx1, - tmpy2 / tmpy1);
 }
 
 vector<vec> p;
@@ -63,7 +57,7 @@ int main()
 	for (auto &it : p) {
 		scanf("%lf %lf", &it.x, &it.y);
 	}
-	
+
 	ansr = INF;
 	for (int i = 0; i < n; i++) {
 		for (int j = i; j < n; j++) {
@@ -88,8 +82,6 @@ int main()
 			}
 		}
 	}
-
 	printf("%.6lf %.6lf %.6lf\n", ans.x, ans.y, ansr);
-
 	return 0;
 }
